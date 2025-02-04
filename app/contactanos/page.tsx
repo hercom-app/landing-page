3845060155d04f90d6650c1c13698f3743daeb19
Mark Pineda <mfpc1500@gmail.com>
2025-02-04 11:13:14 -0500

    fix contactanos banner

--- app/contactanos/page.tsx
+++ app/contactanos/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <main className="relative overflow-hidden bg-neutral-200 dark:bg-neutral-900 w-full min-h-screen -mt-16 pt-16 lg:min-h-[600px] lg:py-16 bg-[url(../assets/nosotros_background.jpg)] bg-cover bg-center">
+      <main className="relative overflow-hidden bg-neutral-200 dark:bg-neutral-900 w-full min-h-screen -mt-16 pt-16 lg:min-h-[600px] lg:py-16 bg-[url(../assets/contactanos_background.jpg)] bg-cover bg-center">
         <div className="absolute z-[2] w-screen top-0 left-0 bg-gradient-to-b from-primary/75 to-transparent h-10" />
         <div className="absolute top-0 left-0 z-[1] bg-white/75 dark:bg-black/75 w-full h-full" />
         <div className="section h-full grid place-items-center relative z-[3]">
